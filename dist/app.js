e6bbd8c3e64267e7de2c123a5cc2a17332934753
SamLinux <oxedom@gmail.com>
2022-08-24 14:20:42 +0300

    tests

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,4 @@
 "use strict";
 
-function sum(a, b) {
-  return a + b;
-}
 
-module.exports = sum;
+module.exports = {};
--- src/app.test.js
+++ src/app.test.js
@@ -1,8 +1,84 @@
-const sum = require('./app.js');
+// const sum = require('./app.js');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+
+
+
+//capitalize
+test('Capitalize 1', () => {
+    expect(capitalize('amit')).toBe('AMIT');
 });
 
+test('Capitalize 2', () => {
+    expect(capitalize('jo3E')).toBe('JO3E');
+});
 
-console.log(expect(sum(1, 2)));
+//reverseString
+test('Reverse String 1 (Numbers)', () => {
+    expect(reverseString('321')).toBe('123');
+});
+
+test('Reverse String 2 (words)', () => {
+    expect(reverseString('mice')).toBe('ecim');
+});
+
+test('Reverse String 3 (numbers and words)', () => {
+    expect(reverseString('PZa3')).toBe('3aZP');
+});
+
+//calculator.add
+test('calculator add 1 ', () => {
+    expect(calculator.add(1, 5)).toBe(6);
+})
+
+test('calculator add 2', () => {
+    expect(calculator.add(-10, 9)).toBe(-1);
+})
+
+test('calculator add 3', () => {
+    expect(calculator.add(0.5, 1.5)).toBe(2);
+})
+
+//calculator.subtract
+test('calculator subtract 1', () => {
+    expect(calculator.subtract(10, 5)).toBe(5);
+})
+
+test('calculator subtract 2', () => {
+    expect(calculator.subtract(-5, -5)).toBe(-10);
+})
+
+test('calculator subtract 3', () => {
+    expect(calculator.subtract(100, 200)).toBe(-100);
+})
+
+//calculator.dvide
+test('calculator divide 1', () => {
+    expect(calculator.divide(20, 4)).toBe(5);
+})
+
+//calculator.dvide
+test('calculator divide 2', () => {
+    expect(calculator.divide(100, 4)).toBe(25);
+})
+
+//calculator.dvide
+test('calculator divide 3', () => {
+    expect(calculator.divide(-5, -2)).toBe(2.5);
+})
+
+//calculator.multiply
+test('calculator multiply 1', () => {
+    expect(calculator.multiply(5, 2)).toBe(10);
+})
+
+test('calculator multiply 2', () => {
+    expect(calculator.multiply(3, 33)).toBe(99);
+})
+test('calculator multiply 3', () => {
+    expect(calculator.multiply(-5, -2)).toBe(10);
+})
+
+
+
+
+
